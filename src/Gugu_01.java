fa9f061c85c2a5ed9df0e1bc023154d11fed9ba7
noblesse9057 <noblesse9057@naver.com>
2019-05-31 16:39:03 +0900

    first

---
+++ src/Gugu_01.java
@@ -0,0 +1,56 @@
+
+public class Gugu_01 {
+
+	public static void main(String[] args) {
+		
+		//备备窜 3窜
+ 		int intNum =1;
+	    
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("3x" + intNum + "=" + 3 * intNum);
+	    
+	    //备备窜 4窜
+	    
+	    intNum =1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    intNum = intNum + 1;
+	    System.out.println("4x" + intNum + "=" + 4 * intNum);
+	    
+	    		
+		    
+	    
+				
+		
+		
+	}
+
+}
